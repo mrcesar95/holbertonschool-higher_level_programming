c508a41f21cc7762f4845c68f16d988f34d65720
Cesar Molina <cesaruxfxtx@gmail.com>
2022-01-03 10:44:49 -0500

    Fixing up files

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
