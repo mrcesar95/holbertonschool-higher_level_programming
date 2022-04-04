2cf6d113548f847740c15c19c890378d883cdb2a
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-04 13:15:35 -0500

    Adding 6-multi_languages_loop.js file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// script that prints 3 lines: (like 1-multi_languages.js) but using an array of string and a loop
+//  script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 do {
