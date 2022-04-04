f7d9289e973f763fc5e5d332facb196b27e919bc
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-04 14:42:54 -0500

    Adding 103-object_fct.js file

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Updating the script by adding a new function: incr
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value += 1;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
