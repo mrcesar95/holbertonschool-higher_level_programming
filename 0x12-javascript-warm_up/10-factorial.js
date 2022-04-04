6acd9befb88a220e8e4b44018bde3f46472b928d
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-04 13:32:04 -0500

    Adding 10-factorial.js file

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that computes the factorial of a number
+
+const { argv } = require('process');
+const n = parseInt(argv[2]);
+
+function factorial(n) {
+  if (n === 0) {
+	return 1;
+  } else {
+	return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(n));
