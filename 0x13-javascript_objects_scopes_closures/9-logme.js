588b8721f612d9ceb3aa5c659b3615f1308ee8ff
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-07 06:40:58 -0500

    Fixing up 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
-#!usr/bin/node
-// function hat prints the number of arguments already printed and the new argument value
+#!/usr/bin/node
+// function that prints the number of arguments already printed and the new argument value
 let counter = 0;
 exports.logMe = function (item) {
   return console.log(`${counter++}: ${item}`);
