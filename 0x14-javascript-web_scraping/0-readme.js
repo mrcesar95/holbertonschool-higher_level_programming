facbfe3fdadd9f83e5211bbc9939794e95079a85
Cesar Molina <cesaruxfxtx@gmail.com>
2022-04-11 15:26:10 -0500

    Adding 0-readme.js, README.md files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const filename = process.argv[2];
+const fs = require("fs");
+
+fs.readFile(filename, function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data.toString("utf8"));
+});
