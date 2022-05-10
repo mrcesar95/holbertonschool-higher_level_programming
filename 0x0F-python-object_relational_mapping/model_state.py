985928342c3a96cd2ef7c3526323213210677c36
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-10 10:38:21 -0500

    Fixing up class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -17,4 +17,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
