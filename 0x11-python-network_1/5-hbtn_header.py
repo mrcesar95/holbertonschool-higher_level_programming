566c292f3836d0812b252562719b2881465f607d
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-19 17:59:16 -0500

    Fixing up 5-hbtn_header.py file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,5 +4,5 @@
 if __name__ == "__main__":
     import requests
     import sys
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("X-Request-Id: {}".format(response.headers.get('X-Request-Id')))
+    response = requests.get('sys.argv[1]')
+    print(response.headers.get("X-Request-Id"))
