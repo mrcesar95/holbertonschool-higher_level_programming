1bc7f429331704bf93f29ef9e641544721b7c848
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-19 18:06:39 -0500

    Adding 6-post_email.py file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""script that sends a POST request to the passed URL"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(response.text)
